e3dfacd4e1d00a5b02f92a6afe1e2780b5d13ec8
sarahjun242 <128445264+sarahjun242@users.noreply.github.com>
2025-03-26 23:27:17 -0400

    fix CORS for mobile

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://sarahsfuncars.vercel.app' // ✅ allow ONLY your frontend
+}));
 app.use(express.json());
 
 const questionRoutes = require('./routes/questions');
